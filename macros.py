627b42b8ffaba542eefd048ed06a7a1a3c64e179
Vijay Rohin Periaiah <vijayrohinp@gmail.com>
2020-02-08 11:13:23 +1100

    Move forward functionality implemented along with validation.

--- macros.py
+++ macros.py
@@ -8,3 +8,6 @@ TOY_TURN_POSITIONS = ["LEFT","RIGHT"]
 
 # Toy Robot Available Main Commands
 TOY_MAIN_COMMANDS = ["PLACE","MOVE","REPORT","LEFT","RIGHT"]
+
+# Toy Robot Operator Functionality
+TOY_OPERATOR_CMD_DICT = { "NORTH": "+", "SOUTH": "-", "EAST": "+", "WEST": "-" }
--- toy_robot.py
+++ toy_robot.py
@@ -6,7 +6,8 @@ DESCRIPTION: * To create the toy_robot with its properties.
              * To check and verify the values of properties whether they are valid or not.
 """
 
-from macros import TOY_ROBOT_DIRECTIONS, TOY_TURN_POSITIONS, TOY_MAIN_COMMANDS
+from macros import TOY_ROBOT_DIRECTIONS, TOY_TURN_POSITIONS, TOY_MAIN_COMMANDS, TOY_OPERATOR_CMD_DICT
+import operator
 
 # Class for Toy-Robot creation and movements
 
@@ -16,6 +17,7 @@ class ToyRobot:
 		self.x_pos = -1
 		self.y_pos = -1
 		self.face_dir = ""
+		self.dir_operators = { "+": operator.add, "-": operator.sub }
 
 	# Setting up the position of Toy Robot on the table (Class function/Member Function)
 	# Returns null
@@ -28,15 +30,32 @@ class ToyRobot:
 	# Returns True/False
 	def validate_place_command(self, gvn_x_pos, gvn_y_pos, gvn_face_dir):
 		is_param_valid = False
-		if( gvn_x_pos > -1 and gvn_x_pos < 5 and gvn_y_pos > -1 and gvn_y_pos < 5 and gvn_face_dir.upper() in TOY_ROBOT_DIRECTIONS ) {
+		if gvn_x_pos > -1 and gvn_x_pos < 5 and gvn_y_pos > -1 and gvn_y_pos < 5 and gvn_face_dir.upper() in TOY_ROBOT_DIRECTIONS:
 			is_param_valid = True
-		}
 		return is_param_valid
 
 	# To check and move the Toy Robot in current direction by one unit place (Class function/Member Function)
 	# Returns True/False
 	def move_forward(self):
-		pass
+		is_move_cmd_valid = True
+		if self.x_pos > -1 and self.y_pos > -1: # To check if toy robot has been placed on the table already
+			# To check if Toy Robot's direction is North and move forward along the column if toy robot doesn't fall off the table (Eg: Orig Place: (3,1) -> MOVE -> Moved Place: (3,2))
+			if TOY_ROBOT_DIRECTIONS[0] == self.face_dir and self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[0]]](self.y_pos, 1) < 5: 
+				self.y_pos = self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[0]]](self.y_pos, 1)
+			# To check if Toy Robot's direction is South and move backward along the column if toy robot doesn't fall off the table (Eg: Orig Place: (3,1) -> MOVE -> Moved Place: (3,0))
+			elif TOY_ROBOT_DIRECTIONS[1] == self.face_dir and self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[1]]](self.y_pos, 1) > -1:
+				self.y_pos = self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[1]]](self.y_pos, 1)
+			# To check if Toy Robot's direction is East and move forward along the row if toy robot doesn't fall off the table (Eg: Orig Place: (3,1) -> MOVE -> Moved Place: (4,1))
+			elif TOY_ROBOT_DIRECTIONS[2] == self.face_dir and self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[2]]](self.x_pos, 1) < 5:
+				self.x_pos = self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[2]]](self.x_pos, 1)
+			# To check if Toy Robot's direction is West and move forward along the row if toy robot doesn't fall off the table (Eg: Orig Place: (3,1) -> MOVE -> Moved Place: (2,1))
+			elif TOY_ROBOT_DIRECTIONS[3] == self.face_dir and self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[3]]](self.x_pos, 1) > -1:
+				self.x_pos = self.dir_operators[TOY_OPERATOR_CMD_DICT[TOY_ROBOT_DIRECTIONS[3]]](self.x_pos, 1)
+			else:
+				is_move_cmd_valid = False
+		else:
+			is_move_cmd_valid = False
+		return is_move_cmd_valid
 
 	# To turn the Toy Robot on either side left or right after verification (Class function/Member Function)
 	# Returns True/False
